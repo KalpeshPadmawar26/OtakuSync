af955558ffdfeb062a124fa164f3c891bf06c5cf
Kalpesh Padmawar <111654076+KalpeshPadmawar26@users.noreply.github.com>
2026-05-04 14:43:11 +0530

    Update app.js with backend redirection

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'https://YOUR_PROJECT_NAME.onrender.com';
+const BACKEND_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'https://otakusync.onrender.com';
 const API_URL = `${BACKEND_URL}/api`;
 
 class App {
